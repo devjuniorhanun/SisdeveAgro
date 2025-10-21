2698b5e490efd5edef7fbd22b1ffa4ca2d6239c9
Winston Hanun Júnior <devjuniorhanun@gmail.com>
2025-10-21 04:56:17 -0300

    Desmembrando o Layout

---
+++ resources/views/layouts/parts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="app-footer">
+    
+    <!--begin::Copyright-->
+    <strong>
+        Copyright &copy; 2025&nbsp;
+        <a href="https://sisdeve.com.br" class="text-decoration-none">Sisdeve</a>.
+    </strong>
+    All rights reserved.
+    <!--end::Copyright-->
+</footer>
